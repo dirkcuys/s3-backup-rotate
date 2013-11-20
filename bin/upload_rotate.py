88f2be912665c7dc6f33a47eef4d5ef5bae6ff22
Dirk Uys <dirkcuys@gmail.com>
2013-11-20 11:10:23 -0300

    Add missing import

--- bin/upload_rotate.py
+++ bin/upload_rotate.py
@@ -4,7 +4,7 @@ import argparse
 import os
 import boto
 from datetime import datetime
-from dcu.active_memory.rotate import rotate
+from dcu.active_memory.rotate import rotate, splitext
 from dcu.active_memory.upload import multipart_upload
 
 parser = argparse.ArgumentParser(description='Upload a file to Amazon S3 and rotate old backups.')
