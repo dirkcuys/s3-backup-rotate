f224a76fc11279f20ad201ba94da15664d3dec06
Dirk Uys <dirkcuys@gmail.com>
2013-11-20 11:04:28 -0300

    Update for pypi packaging

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='dcu.active-memory',
-    version='0.1.4',
+    version='0.1.5',
     author='Dirk Uys',
     author_email='dirkcuys@gmail.com',
-    packages=['dcu.active_memory'],
+    packages=['dcu', 'dcu.active_memory'],
     scripts=['bin/upload_rotate.py'],
     url='https://github.com/dirkcuys/active-memory',
     license='LICENSE.txt',
