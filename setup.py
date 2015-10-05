51d526a7ee0933af55e2d42d8642c78a53746982
Dirk Uys <dirkcuys@gmail.com>
2015-10-05 15:08:06 -0600

    Update packing

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
-from distutils.core import setup
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+ 
+here = path.abspath(path.dirname(__file__))    
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='dcu.active-memory',
-    version='0.1.7',
+    version='0.1.9',
     author='Dirk Uys',
     author_email='dirkcuys@gmail.com',
     packages=['dcu', 'dcu.active_memory'],
@@ -10,7 +19,7 @@ setup(
     url='https://github.com/dirkcuys/active-memory',
     license='LICENSE.txt',
     description='Script rotate backup files on AWS S3 according to a grandfather, father, sun strategy.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     install_requires=[
         "boto >= 2.8.0",
         "filechunkio >= 1.5",
